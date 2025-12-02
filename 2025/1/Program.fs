b06526da00a77148a2e1611bc3f2af087793b7bd
George Doenlen <gdoenlen@gmail.com>
2025-12-02 10:53:40 -0800

    Better Day 1 Part 1

--- 2025/1/Program.fs
+++ 2025/1/Program.fs
@@ -4,47 +4,58 @@ open System.IO
 let parseLine (line: string) =
   let amount = Int32.Parse line[1..]
   match line[0] with
-  | 'L' -> -amount
-  | 'R' -> amount
+  | 'L' -> amount
+  | 'R' -> -amount
   | _ -> failwith "Not Left or Right?"
 
 let parseFile =
-  File.ReadLines("./input.txt")
+  File.ReadLines "./input.txt"
    |> Seq.map parseLine
 
-type State = {
-  current: int
-  next: int
-  count: int
-  move: int
-} with
-  member this.numRotations = abs this.move / 100
+type Dial() =
+  member val index = 50 with get, private set
+  member this.rotate(amount: int) =
+    let actual = amount % 100
+    this.index <- this.index + actual
+    if this.index < 0 then
+      this.index <- this.index + 100
+    else if this.index >= 100 then
+      this.index <- this.index - 100
 
-let count moves (fn: State -> int) =
-  let _, count =
-    moves
-      |> Seq.fold
-        (fun (index, count) move ->
-          let next = (index + move) % 100
-          let mutable count = fn {
-            current = index
-            next = next
-            count = if next = 0 then count + 1 else count
-            move = move
-          }
-          (next, count))
-        (50, 0)
-  count
+let partTwo (moves: seq<int>) =
+  let dial = Dial()
+  moves
+    |> Seq.fold
+      (fun (count) move ->
+        let current = dial.index
+        let actual = move % 100
+        let rotations = abs (move / 100)
+        dial.rotate actual
 
-let partTwo (state: State) =
-  let mutable count = state.count + state.numRotations
-  if state.next < 0 && state.current > 0 then
-    count <- count + 1
-  if state.next > 0 && state.current < 0 then
-    count <- count + 1
-  count
+        let mutable next = count + rotations
+        if dial.index = 0 then
+          next <- next + 1
+        if move < 0 && dial.index > current then
+          next <- next + 1
+        if move > 0 && dial.index < current then
+          next <- next + 1
+        next
+      )
+      0
 
+let partOne (moves: seq<int>) =
+  let dial = Dial()
+  moves
+    |> Seq.fold
+      (fun (count) move ->
+        dial.rotate move
+        if dial.index = 0 then
+          count + 1
+        else
+          count
+      )
+      0
 
 let moves = parseFile
-printfn "part one %d" (count moves (fun state -> state.count))
-printfn "part two %d" (count moves partTwo)
+printfn "part one %d" (partOne moves)
+printfn "part two %d" (partTwo moves)
