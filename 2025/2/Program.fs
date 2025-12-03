0b48f493cfb20c02e10db11848d298e619b6285d
George Doenlen <gdoenlen@gmail.com>
2025-12-02 18:47:07 -0800

    Better Day 2 Part 1

--- 2025/2/Program.fs
+++ 2025/2/Program.fs
@@ -1,32 +1,31 @@
 ﻿open System
 open System.IO
 
-type Id(value: string) =
+type Id(text: string, value: Int64) =
   member this.isFake =
-    if Int32.IsEvenInteger value.Length then
-      let middle = value.Length / 2 // 0 based middle
-      let left = value.AsSpan(0, middle)
-      let right = value.AsSpan middle
+    if Int32.IsEvenInteger text.Length then
+      let middle = text.Length / 2 // 0 based middle
+      let left = text.AsSpan(0, middle)
+      let right = text.AsSpan middle
       left.SequenceEqual right
     else
       false
-  member this.value: Int64 =
-    Int64.Parse value
+  member this.value: Int64 = value
+
+let toRange (s: string) =
+  let parts = s.Split("-")
+  let start = Int64.Parse parts[0]
+  let last= Int64.Parse parts[1]
+  [start..last]
 
 let ids =
   let text = File.ReadAllText "./input.txt"
   text.Split(",")
     |> Array.toSeq
+    |> Seq.map toRange
+    |> Seq.collect id
     |> Seq.map
-      (fun id ->
-        let parts = id.Split("-")
-        let start = Int64.Parse parts[0]
-        let last= Int64.Parse parts[1]
-        [start..last])
-    |> Seq.collect
-      (fun lst -> lst)
-    |> Seq.map
-      (fun id -> Id (string id))
+      (fun id -> Id (string id, id))
 
 let total =
   ids
