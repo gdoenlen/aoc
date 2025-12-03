e4b465055c4b4d601c318511ef1f32c228952ed2
George Doenlen <gdoenlen@gmail.com>
2025-12-02 19:55:09 -0800

    Begin Day 2 Part 2

--- 2025/2/Program.fs
+++ 2025/2/Program.fs
@@ -1,16 +1,26 @@
 ﻿open System
 open System.IO
 
-type Id(text: string, value: Int64) =
-  member this.isFake =
-    if Int32.IsEvenInteger text.Length then
-      let middle = text.Length / 2 // 0 based middle
-      let left = text.AsSpan(0, middle)
-      let right = text.AsSpan middle
-      left.SequenceEqual right
-    else
-      false
-  member this.value: Int64 = value
+type Id =
+  abstract member isFake: bool
+  abstract member value: Int64
+
+type PartOneId (text: string, value: Int64) =
+  interface Id with
+    member this.isFake =
+      if Int32.IsEvenInteger text.Length then
+        let middle = text.Length / 2 // 0 based middle
+        let left = text.AsSpan(0, middle)
+        let right = text.AsSpan middle
+        left.SequenceEqual right
+      else
+        false
+    member this.value: Int64 = value
+
+type PartTwoId (text: String, value: Int64) =
+  interface Id with
+    member this.isFake = failwith "todo"
+    member this.value = value
 
 let toRange (s: string) =
   let parts = s.Split("-")
@@ -18,16 +28,23 @@ let toRange (s: string) =
   let last= Int64.Parse parts[1]
   [start..last]
 
-let total =
+type Part = One | Two
+
+let toId (id: Int64, part: Part): Id =
+  match part with
+  | One -> PartOneId (string id, id)
+  | Two -> PartTwoId (string id, id)
+
+let total part =
   File.ReadAllText "./input.txt"
     |> _.Split(",")
     |> Array.toSeq
     |> Seq.map toRange
     |> Seq.collect id
     |> Seq.map
-      (fun id -> Id (string id, id))
+      (fun id -> toId (id, part))
     |> Seq.filter _.isFake
     |> Seq.map _.value
     |> Seq.sum
 
-printfn "%d" total
+printfn "%d" (total One)
