69c773772eaf147b05b857441b7c731c4bb7b3cf
George Doenlen <gdoenlen@gmail.com>
2025-12-04 09:37:12 -0800

    unify searching

--- 2025/3/Program.fs
+++ 2025/3/Program.fs
@@ -4,34 +4,22 @@ open System.IO
 let toInt ch = int (Char.GetNumericValue ch)
 
 type Battery (value: string) =
-  let maxL =
-    let mutable i= 1
-    let mutable l = toInt value[0]
-    let mutable lindex = 0
-    while i < value.Length - 2 do
-      let v = toInt value[i]
-      if v > l then
-        l <- v
-        lindex <- i
-      i <- i + 1
-    lindex
-
-  let maxR start =
-    let mutable r = toInt value[start]
-    let mutable rindex = start
+  let findMax start stop =
+    let mutable v = toInt value[start]
+    let mutable vindex = start
     let mutable i = start + 1
-    while i < value.Length - 1 do
-      let v = toInt value[i]
-      if v > r then
-        r <- v
-        rindex <- i
+    while i < stop do
+      let t = toInt value[i]
+      if t > v then
+        v <- t
+        vindex <- i
       i <- i + 1
-    rindex
+    vindex
 
   member this.maxJolts: int =
-    let lindex = maxL
+    let lindex = findMax 0 (value.Length - 2)
     let l = string value[lindex]
-    let rindex = maxR (lindex + 1)
+    let rindex = findMax (lindex + 1) (value.Length - 1)
     let r = string value[rindex]
     Int32.Parse (l + r)
 
