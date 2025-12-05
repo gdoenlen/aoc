58f27d6c37d8283ea36c17ef4e2a95b57123f9f8
George Doenlen <gdoenlen@gmail.com>
2025-12-04 18:33:38 -0800

    Day 3 Part 2

--- 2025/3/Program.fs
+++ 2025/3/Program.fs
@@ -1,14 +1,15 @@
 ﻿open System
+open System.Collections.Generic
 open System.IO
 
 let toInt ch = int (Char.GetNumericValue ch)
 
-type Battery (value: string) =
+type Battery (value: string, numSwitches: int) =
   let findMax start stop =
     let mutable v = toInt value[start]
     let mutable vindex = start
     let mutable i = start + 1
-    while i <= stop do
+    while i < stop do
       let t = toInt value[i]
       if t > v then
         v <- t
@@ -16,19 +17,23 @@ type Battery (value: string) =
       i <- i + 1
     vindex
 
-  member this.maxJolts: int =
-    let lindex = findMax 0 (value.Length - 2)
-    let l = string value[lindex]
-    let rindex = findMax (lindex + 1) (value.Length - 1)
-    let r = string value[rindex]
-    Int32.Parse (l + r)
+  member this.maxJolts: Int64 =
+    let digits = List<String>()
+    let mutable previousIndex = 0
+    let switches = List.rev [0..(numSwitches - 1)]
+    for i in switches do
+      let index = findMax previousIndex (value.Length - i)
+      digits.Add (string value[index])
+      previousIndex <- (index + 1)
+    Int64.Parse (String.Join ("", digits))
 
-let total =
+let total numSwitches =
   File.ReadAllLines "./input.txt"
     |> Array.toSeq
     |> Seq.map
-      (fun batt -> Battery batt)
+      (fun batt -> Battery (batt, numSwitches))
     |> Seq.map _.maxJolts
     |> Seq.sum
 
-printfn "%d" total
+printfn "%d" (total 2)
+printfn "%d" (total 12)
