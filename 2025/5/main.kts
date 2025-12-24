096f2ddb2975e3e4eb37358598e4cd20f0d2d1d1
George Doenlen <gdoenlen@gmail.com>
2025-12-24 09:12:57 -0800

    Use built in ranges

--- 2025/5/main.kts
+++ 2025/5/main.kts
@@ -1,25 +1,23 @@
 import java.nio.file.Files
 import kotlin.io.path.Path
 
-class Range(val start: Long, val end: Long) {
-  fun isWithin(l: Long) = this.start <= l && l <= this.end
-}
-
-class Ingredients(private val ranges: List<Range>) {
-  fun isFresh(id: Long) = this.ranges.find { it.isWithin(id) } != null
+class Ingredients(private val ranges: List<LongRange>) {
+  fun isFresh(id: Long) = this.ranges.find { it.contains(id) } != null
 }
 
 val ingredients = Ingredients(
   Files.readAllLines(Path("./ranges.txt"))
     .asSequence()
     .map(::toRange)
-    .sortedBy(Range::start)
     .toList()
 )
 
-fun toRange(line: String): Range {
+fun toRange(line: String): LongRange {
   val parts = line.split("-")
-  return Range(parts[0].toLong(), parts[1].toLong())
+  val start = parts[0].toLong()
+  val end = parts[1].toLong()
+
+  return start..end
 }
 
 val sum = Files.readAllLines(Path("./ids.txt"))
